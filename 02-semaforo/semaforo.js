e27714ddb4be55e083f21de214ca21801d4fa1eb
root <fernandoleonid@gmail.com>
2020-11-07 16:55:26 -0300

    Projeto semaforo

---
+++ 02-semaforo/semaforo.js
@@ -0,0 +1,32 @@
+const img = document.getElementById( 'img' );
+const buttons = document.getElementById( 'buttons' );
+let colorIndex = 0;
+let intervalId = null;
+
+const trafficLight = ( event ) => {
+    stopAutomatic();
+    turnOn[event.target.id]();
+}
+
+const nextIndex = () => colorIndex = colorIndex < 2 ? ++colorIndex : 0;
+
+const changeColor = () => {
+    const colors = ['red','yellow','green']
+    const color = colors[ colorIndex ];
+    turnOn[color]();
+    nextIndex();
+}
+
+const stopAutomatic = () => {
+    clearInterval ( intervalId );
+}
+
+const turnOn = {
+    'red':      () => img.src = './img/vermelho.png',
+    'yellow':   () => img.src = './img/amarelo.png',
+    'green':    () => img.src = './img/verde.png',
+    'automatic': () => intervalId = setInterval( changeColor, 1000 )
+}
+
+buttons.addEventListener('click', trafficLight );
+
