66860079e9f76eb5053f7a169cb0ca70a01dbfeb
fernandoleonid <fernandoleonid@gmail.com>
2020-12-04 00:50:50 -0300

    Solução desafio da lâmpada

---
+++ 01-Solução lamp/lampada.js
@@ -0,0 +1,41 @@
+const turnOnOff = document.getElementById ( 'turnOnOff' );
+
+const lamp = document.getElementById ( 'lamp' );
+
+function isLampBroken () {
+    return lamp.src.indexOf ( 'quebrada' ) > -1
+}
+
+function lampOn () {
+    if ( !isLampBroken () ) {
+        lamp.src = './img/ligada.jpg';
+    }
+}
+
+function lampOff () {
+    if ( !isLampBroken () ) {
+        lamp.src = './img/desligada.jpg';
+    }
+}
+
+function lampBroken () {
+    lamp.src = './img/quebrada.jpg';
+}
+
+function lampOnOff () {
+    if ( turnOnOff.textContent == 'Ligar' ) {
+        lampOn();
+        turnOnOff.textContent = 'Desligar';
+    }else{
+        lampOff();
+        turnOnOff.textContent = 'Ligar';
+    }
+}
+
+
+turnOnOff.addEventListener ( 'click', lampOnOff );
+
+lamp.addEventListener ( 'mouseover', lampOn );
+lamp.addEventListener ( 'mouseleave', lampOff );
+lamp.addEventListener ( 'dblclick', lampBroken );
+
