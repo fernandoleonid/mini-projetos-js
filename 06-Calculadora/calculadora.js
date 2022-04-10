97822222f0e43a58416e4c3e240aa0e0902836a9
fernandoleonid <fernandoleonid@gmail.com>
2022-04-10 10:32:57 -0300

    fix error calculation with thousands

--- 06-Calculadora/calculadora.js
+++ 06-Calculadora/calculadora.js
@@ -11,104 +11,111 @@ let numeroAnterior;
 const operacaoPendente = () => operador !== undefined;
 
 const calcular = () => {
-    if (operacaoPendente()){
-        const numeroAtual = parseFloat(display.textContent.replace(',','.'));
+    if (operacaoPendente()) {
+        const numeroAtual = parseFloat(display.textContent.replace('.','').replace(',', '.'));
         novoNumero = true;
-        const resultado = eval (`${numeroAnterior}${operador}${numeroAtual}`);
+        const resultado = eval(`${numeroAnterior}${operador}${numeroAtual}`);
         atualizarDisplay(resultado);
     }
-}
+};
 
 const atualizarDisplay = (texto) => {
-    if (novoNumero){
+    if (novoNumero) {
         display.textContent = texto.toLocaleString('BR');
         novoNumero = false;
-    }else{
+    } else {
         display.textContent += texto.toLocaleString('BR');
     }
-}
+    document.querySelector('#igual').focus();
+};
 
 const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent);
-numeros.forEach (numero => numero.addEventListener('click', inserirNumero));
-
+numeros.forEach((numero) => numero.addEventListener('click', inserirNumero));
 
 const selecionarOperador = (evento) => {
     if (!novoNumero) {
         calcular();
         novoNumero = true;
         operador = evento.target.textContent;
-        numeroAnterior = parseFloat(display.textContent.replace(',','.'));
+        numeroAnterior = parseFloat(display.textContent.replace('.','').replace(',', '.'));
     }
-}
-operadores.forEach (operador => operador.addEventListener('click', selecionarOperador));
+};
+operadores.forEach((operador) =>
+    operador.addEventListener('click', selecionarOperador)
+);
 
 const ativarIgual = () => {
     calcular();
     operador = undefined;
-}
+};
 document.getElementById('igual').addEventListener('click', ativarIgual);
 
-const limparDisplay = () => display.textContent = '';
-document.getElementById('limparDisplay').addEventListener('click', limparDisplay);
+const limparDisplay = () => (display.textContent = '');
+document
+    .getElementById('limparDisplay')
+    .addEventListener('click', limparDisplay);
 
 const limparCalculo = () => {
     limparDisplay();
     operador = undefined;
     novoNumero = true;
     numeroAnterior = undefined;
-}
-document.getElementById('limparCalculo').addEventListener('click', limparCalculo);
+};
+document
+    .getElementById('limparCalculo')
+    .addEventListener('click', limparCalculo);
 
-const removerUltimoNumero = () => display.textContent = display.textContent.slice(0, -1);
-document.getElementById('backspace').addEventListener('click', removerUltimoNumero);
+const removerUltimoNumero = () =>
+    (display.textContent = display.textContent.slice(0, -1));
+document
+    .getElementById('backspace')
+    .addEventListener('click', removerUltimoNumero);
 
 const inverterSinal = () => {
-    novoNumero = true
-    atualizarDisplay (display.textContent * -1);
-}
+    novoNumero = true;
+    atualizarDisplay(display.textContent * -1);
+};
 document.getElementById('inverter').addEventListener('click', inverterSinal);
-
 
 const existeDecimal = () => display.textContent.indexOf(',') !== -1;
 const existeValor = () => display.textContent.length > 0;
 const inserirDecimal = () => {
-    if (!existeDecimal()){
-        if (existeValor()){
-            atualizarDisplay(',');
-        }else{
+    if (!existeDecimal()) {
+        if (novoNumero) {
             atualizarDisplay('0,');
+        } else {
+            atualizarDisplay(',');
         }
     }
-}
+};
 document.getElementById('decimal').addEventListener('click', inserirDecimal);
 
-
 const mapaTeclado = {
-    '0'         : 'tecla0',
-    '1'         : 'tecla1',
-    '2'         : 'tecla2',
-    '3'         : 'tecla3',
-    '4'         : 'tecla4',
-    '5'         : 'tecla5',
-    '6'         : 'tecla6',
-    '7'         : 'tecla7',
-    '8'         : 'tecla8',
-    '9'         : 'tecla9',
-    '/'         : 'operadorDividir',
-    '*'         : 'operadorMultiplicar',
-    '-'         : 'operadorSubtrair',
-    '+'         : 'operadorAdicionar',
-    '='         : 'igual',
-    'Enter'     : 'igual',
-    'Backspace' : 'backspace',
-    'c'         : 'limparDisplay',
-    'Escape'    : 'limparCalculo',
-    ','         : 'decimal'
-}
+    0: 'tecla0',
+    1: 'tecla1',
+    2: 'tecla2',
+    3: 'tecla3',
+    4: 'tecla4',
+    5: 'tecla5',
+    6: 'tecla6',
+    7: 'tecla7',
+    8: 'tecla8',
+    9: 'tecla9',
+    '/': 'operadorDividir',
+    '*': 'operadorMultiplicar',
+    '-': 'operadorSubtrair',
+    '+': 'operadorAdicionar',
+    '=': 'igual',
+    Enter: 'igual',
+    Backspace: 'backspace',
+    c: 'limparDisplay',
+    Escape: 'limparCalculo',
+    ',': 'decimal',
+};
 
 const mapearTeclado = (evento) => {
     const tecla = evento.key;
     const teclaPermitida = () => Object.keys(mapaTeclado).indexOf(tecla) !== -1;
-    if (teclaPermitida())  document.getElementById(mapaTeclado[tecla]).click();
-}
+    if (teclaPermitida()) document.getElementById(mapaTeclado[tecla]).click();
+};
 document.addEventListener('keydown', mapearTeclado);
