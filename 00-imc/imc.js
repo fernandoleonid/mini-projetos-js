c072f174e3ea214a63b0308676deac2b2bcaacc4
fernandoleonid <fernandoleonid@gmail.com>
2022-12-03 06:36:23 -0300

    clear space

--- 00-imc/imc.js
+++ 00-imc/imc.js
@@ -2,8 +2,8 @@ const calcular = document.getElementById('calcular');
 
 function imc () {
     const nome = document.getElementById('nome').value;
-    const altura = document.getElementById('altura').value;
-    const peso = document.getElementById('peso').value;
+    const altura = +document.getElementById('altura').value;
+    const peso = +document.getElementById('peso').value;
     const resultado = document.getElementById('resultado');
 
     if (nome !== '' && altura !== '' && peso !== '') {
